cba1fd95d95b28af89e4770e26c6a161f166428a
DBugDD <tawab.bug@gmail.com>
2025-04-15 16:52:04 +0600

    Binary-Search

--- 3rd_semester/DSA_Lab/Linear-Search/Linear-Search.cpp
+++ 3rd_semester/DSA_Lab/Linear-Search/Linear-Search.cpp
@@ -19,7 +19,7 @@ int main(){
 
     int arr[size];
 
-    cout << "Enter " << size << "elements";
+    cout << "Enter " << size << "elements: ";
     for(int i=0; i<size; i++){
         cin >> arr[i];
     }
